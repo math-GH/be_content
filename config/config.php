2ca9f86b1b92be321aaa7d9e5f4f037e62f9a93f
math <math@maTHINKing>
2016-07-10 22:48:30 +0200

    fixed: css usage

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?
+
+if (!defined('TL_ROOT')) {
+  die('You cannot access this file directly!');
+}
+
+/**
+ * Extension for Contao Open Source CMS
+ *
+ * PHP version 5
+ * @copyright  µaTh 2016 
+ * @author     µaTh
+ * @package    be_content
+ * @license    GNU
+ * @filesource https://github.com/mathContao/be_content
+ */
+
+$GLOBALS['TL_CSS'][] = "./system/modules/be_content/assets/be_content.css";
